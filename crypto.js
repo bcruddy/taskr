e25ab6dba8d45510071d9c6db63002daf7b4b0b9
brian ruddy <briancruddy@gmail.com>
2017-12-05 14:22:49 -0500

    get value of crypto holdings

--- crypto.js
+++ crypto.js
@@ -1,16 +1,83 @@
 const fetch = require('node-fetch');
+const HOLDINGS = require('./crypto-holdings')
 
-const btc = fetch('https://api.gemini.com/v1/pubticker/btcusd').then(r => r.json());
-const eth = fetch('https://api.gemini.com/v1/pubticker/ethusd').then(r => r.json());
-const ltc = fetch('https://api.cryptonator.com/api/ticker/ltc-usd').then(r => r.json());
-
-Promise.all([btc, eth, ltc])
-    .then(([btc, eth, ltc]) => {
-        console.log(
-            `BTC: $${btc.last}`,
-            `\nETH: $${eth.last}`,
-            `\nLTC: $${parseFloat(ltc.ticker.price, 10).toFixed(2)}`
-        );
-
+getPrices()
+    .then(() => {
         process.exit(0);
     });
+
+function getPrices () {
+    const baseEndpoint = 'https://api.cryptonator.com/api/ticker';
+    const btc = fetch(`${baseEndpoint}/btc-usd`).then(r => r.json()),
+        eth = fetch(`${baseEndpoint}/eth-usd`).then(r => r.json()),
+        ltc = fetch(`${baseEndpoint}/ltc-usd`).then(r => r.json()),
+        xrp = fetch(`${baseEndpoint}/xrp-usd`).then(r => r.json()),
+        neo = fetch(`${baseEndpoint}/neo-usd`).then(r => r.json()),
+        vtc = fetch(`${baseEndpoint}/vtc-usd`).then(r => r.json());
+
+    return Promise.all([btc, eth, ltc, xrp, neo, vtc])
+        .then(buildPricingObject)
+        .then(formatPricingData)
+        .then(logPricing);
+}
+
+function buildPricingObject([btc, eth, ltc, xrp, neo, vtc]) {
+    return {
+        btc: {
+            symbol: 'BTC',
+            price: parseFloat(btc.ticker.price, 10).toFixed(2),
+            holdings: HOLDINGS.btc.toFixed(8),
+            value: parseFloat(btc.ticker.price * HOLDINGS.btc, 10).toFixed(2)
+        },
+        eth: {
+            symbol: 'ETH',
+            price: parseFloat(eth.ticker.price, 10).toFixed(2),
+            holdings: HOLDINGS.eth.toFixed(8),
+            value: parseFloat(eth.ticker.price * HOLDINGS.eth, 10).toFixed(2)
+        },
+        ltc: {
+            symbol: 'LTC',
+            price: parseFloat(ltc.ticker.price, 10).toFixed(2),
+            holdings: HOLDINGS.ltc.toFixed(8),
+            value: parseFloat(ltc.ticker.price * HOLDINGS.ltc, 10).toFixed(2)
+        },
+        xrp: {
+            symbol: 'XRP',
+            price: parseFloat(xrp.ticker.price, 10).toFixed(5),
+            holdings: HOLDINGS.xrp.toFixed(6),
+            value: parseFloat(xrp.ticker.price * HOLDINGS.xrp, 10).toFixed(2)
+        },
+        neo: {
+            symbol: 'NEO',
+            price: parseFloat(neo.ticker.price, 10).toFixed(5),
+            holdings: HOLDINGS.neo.toFixed(8),
+            value: parseFloat(neo.ticker.price * HOLDINGS.neo, 10).toFixed(2)
+        },
+        vtc: {
+            symbol: 'VTC',
+            price: parseFloat(vtc.ticker.price, 10).toFixed(5),
+            holdings: HOLDINGS.vtc.toFixed(7),
+            value: parseFloat(vtc.ticker.price * HOLDINGS.vtc, 10).toFixed(2)
+        }
+    };
+}
+
+function formatPricingData (info) {
+    return Object.entries(info)
+        .reduce((acc, [, currency]) => {
+            const {symbol, price, holdings, value} = currency;
+
+            acc.list.push(`${symbol}: $${price}\t${holdings}\t$${value}`);
+            acc.sum += parseFloat(value, 10);
+
+            return acc;
+        }, { sum: 0, list: []});
+}
+
+function logPricing ({list, sum}) {
+    console.log(
+        list.join('\n'),
+        `\n\t\t\t\t--------`,
+        `\n\t\t\t\t$${sum.toFixed(2)}`
+    );
+}
