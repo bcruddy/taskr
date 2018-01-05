1cf4bd3ffcab6ec5dea420a8563fe8dc83efe846
brian ruddy <briancruddy@gmail.com>
2018-01-04 21:02:36 -0500

    formatting and new currencies

--- crypto.js
+++ crypto.js
@@ -9,15 +9,15 @@ getPrices()
 function getPrices () {
     const baseEndpoint = 'https://api.coinmarketcap.com/v1/ticker';
     const btc = fetch(`${baseEndpoint}/bitcoin`).then(r => r.json()),
+        bch = fetch(`${baseEndpoint}/bitcoin-cash`).then(r => r.json()),
         eth = fetch(`${baseEndpoint}/ethereum`).then(r => r.json()),
         ltc = fetch(`${baseEndpoint}/litecoin`).then(r => r.json()),
-        xrp = fetch(`${baseEndpoint}/ripple`).then(r => r.json()),
-        vtc = fetch(`${baseEndpoint}/vertcoin`).then(r => r.json()),
-        iota = fetch(`${baseEndpoint}/iota`).then(r => r.json()),
         ada = fetch(`${baseEndpoint}/cardano`).then(r => r.json()),
-        trx = fetch(`${baseEndpoint}/tronix`).then(r => r.json());
+        xrp = fetch(`${baseEndpoint}/ripple`).then(r => r.json()),
+        trx = fetch(`${baseEndpoint}/tron`).then(r => r.json()),
+        xlm = fetch(`${baseEndpoint}/stellar`).then(r => r.json());
 
-    return Promise.all([btc, eth, ltc, xrp, vtc, iota, ada, trx])
+    return Promise.all([btc, bch, eth, ltc, ada, xrp, xlm, trx])
         .then(buildPricingObject)
         .then(formatPricingData)
         .then(logPricing);
@@ -25,14 +25,19 @@ function getPrices () {
 
 function buildPricingObject(data) {
     return data.reduce((memo, item) => {
-        const {id, symbol, price_usd: price} = item[0];
+        const {id, symbol, price_usd: price, rank} = item[0];
         const currency = symbol.toLowerCase();
+
+        if (!HOLDINGS[currency]) {
+            return memo;
+        }
 
         memo[currency] = {
             symbol,
+            rank,
             price: parseFloat(price, 10).toFixed(symbol === 'BTC' ? 2 : 5),
             holdings: HOLDINGS[currency].toFixed(8),
-            value: parseFloat(price * HOLDINGS[currency], 10).toFixed(2)
+            value: parseFloat(price * HOLDINGS[currency], 10).toFixed(2),
         };
 
         return memo;
@@ -41,24 +46,30 @@ function buildPricingObject(data) {
 
 function formatPricingData (info) {
     return Object.entries(info)
+        .sort((a, b) => parseInt(a[1].rank, 10) > parseInt(b[1].rank, 10) ? 1 : -1)
         .reduce((acc, [, currency]) => {
-            let {symbol, price, holdings, value} = currency;
+            let {symbol, price, holdings, value, rank} = currency;
 
-            if (symbol === 'MIOTA') {
-                symbol = 'IOT';
+            if (value < 5) {
+                return acc;
             }
 
-            acc.list.push(`${symbol}: $${price}\t${holdings}\t$${value}`);
+            if (symbol === 'STRAT') {
+                symbol = 'STR';
+            }
+
+            acc.list.push(`${symbol} ${rank}\t$${price}\t${holdings}\t$${value}`);
             acc.sum += parseFloat(value, 10);
 
             return acc;
-        }, { sum: 0, list: []});
+        }, {sum: 0, list: []});
 }
 
 function logPricing ({list, sum}) {
+    console.log(`${new Date()}\n`);
     console.log(
-        list.join('\n'),
-        `\n\t\t\t\t--------`,
-        `\n\t\t\t\t$${sum.toFixed(2)}`
+        list.join('\n').trim(),
+        `\n\t\t\t\t\t--------`,
+        `\n\t\t\t\t\t$${sum.toFixed(2)}`
     );
 }
